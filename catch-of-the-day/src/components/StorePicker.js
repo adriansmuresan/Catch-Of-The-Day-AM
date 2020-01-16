d43b12824ec1530d106dc454fd08f5b14d7d9e4d
Adrian S MUresan <adriansmuresan@gmail.com>
2020-01-15 18:25:30 -0600

    Removes action attribute

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -3,7 +3,7 @@ import React from 'react';
 class StorePicker extends React.Component {
   render() {
     return (
-    <form action="" className="store-selector">
+    <form className="store-selector">
       <h2>Please Enter A Store</h2>
       <input type="text" requiered placeholder="Store Name"/>
       <button type="submit">Visit Store →</button>
