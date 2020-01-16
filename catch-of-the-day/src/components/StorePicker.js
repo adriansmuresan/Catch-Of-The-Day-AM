c2418312eb893b6970639c1f9f1ee98fc9d04adf
Adrian S MUresan <adriansmuresan@gmail.com>
2020-01-16 14:22:23 -0600

    Adds an event to the form with a method and initializes a ref on the input

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,11 +2,19 @@ import React from 'react';
 import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+  myInput = React.createRef();
+
+  goToStore(e) {
+    // Stop the form from submitting
+    e.preventDefault();
+    //  Get the text from that input
+    // Change the page to /store/whatever-they-entered
+  }
   render() {
     return (
-    <form className="store-selector">
+    <form className="store-selector" onSubmit={this.goToStore}>
       <h2>Please Enter A Store</h2>
-      <input type="text" requiered placeholder="Store Name" defaultValue={getFunName()} />
+      <input type="text" ref={this.myInput} requiered placeholder="Store Name" defaultValue={getFunName()} />
       <button type="submit">Visit Store →</button>
     </form>
     )
