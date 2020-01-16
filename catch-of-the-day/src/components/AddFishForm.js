ef064cdccefea0d6bd4d325e4bde500c951ffc95
Adrian S MUresan <adriansmuresan@gmail.com>
2020-01-16 15:34:57 -0600

    Creates the addFishForm component

---
+++ catch-of-the-day/src/components/AddFishForm.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class AddFishForm extends React.Component {
+  render() {
+    return(
+      <form>
+        <p>IT Works!!</p>
+      </form>
+    );
+  }
+}
+
+export default AddFishForm;
