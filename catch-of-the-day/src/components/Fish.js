b6d65b04bf976c9b0256d5354bf1da5a7efef0d8
Adrian S MUresan <adriansmuresan@gmail.com>
2020-01-17 14:08:22 -0600

    Creates a fish component

---
+++ catch-of-the-day/src/components/Fish.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Fish extends React.Component {
+  render() {
+    return (
+      <div className="single0-fish">Fish🐟!!!</div>
+    )
+  }
+}
+
+export default Fish;
