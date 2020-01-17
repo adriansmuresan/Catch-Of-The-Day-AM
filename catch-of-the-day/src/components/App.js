3b31282c879a5d86cf618afc2c80951bb423fadb
Adrian S MUresan <adriansmuresan@gmail.com>
2020-01-17 14:11:00 -0600

    Imports the fish, adds an unordered list and loops over the fishes object in order to get each fish

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -3,12 +3,13 @@ import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
 import sampleFishes from '../sample-fishes'
+import Fish from './Fish';
+
 
 class App extends React.Component {
   state = {
     fishes: {},
-    order: {},
-
+    order: {}
   };
 
   addFish = fish => {
@@ -32,7 +33,10 @@ class App extends React.Component {
     return(
       <div className="catch-of-the-day">
         <div className="menu">
-          <Header tagline="Fresh Seafood Market"/>          
+          <Header tagline="Fresh Seafood Market"/>
+          <ul className="fishes">
+            {Object.keys(this.state.fishes).map(key => <Fish key={key} />)}
+          </ul>     
         </div>
         <Order />
         <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes} />
