f0195f3563cf99b202d0e726525b80ced1f7d2f4
Adrian S MUresan <adriansmuresan@gmail.com>
2020-01-20 14:16:04 -0600

    Creates a list and loops over the order ids

--- catch-of-the-day/src/components/Order.js
+++ catch-of-the-day/src/components/Order.js
@@ -17,7 +17,7 @@ class Order extends React.Component {
     return (
       <div className="order-wrap">
         <h2>Order</h2>
-        {orderIds}
+        <ul>{orderIds.map(key => <li>{key}</li>)}</ul>
         <div className="total">
           Total:
           <strong>{formatPrice(total)}</strong>
