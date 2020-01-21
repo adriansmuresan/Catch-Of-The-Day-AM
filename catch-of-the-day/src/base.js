870cb3ea819dbf0928a4e9e7b89b625d19f540a7
Adrian S MUresan <adriansmuresan@gmail.com>
2020-01-21 14:25:26 -0600

    Creates the base.js file where we created the firebaseApp and used Rebase to bing to our firebaseApp

---
+++ catch-of-the-day/src/base.js
@@ -0,0 +1,10 @@
+import Rebase from 're-base';
+import firebase from 'firebase';
+
+const firebaseApp = firebase.initializeApp({
+  apiKey: "AIzaSyCAiXMBhr62HqFbH294MIDHeWAPxurcuHI",
+  authDomain: "catch-of-the-day-adrian-87833.firebaseapp.com",
+  databaseURL: "https://catch-of-the-day-adrian-87833.firebaseio.com"
+});
+
+const base = Rebase.createClass(firebaseApp.database());
