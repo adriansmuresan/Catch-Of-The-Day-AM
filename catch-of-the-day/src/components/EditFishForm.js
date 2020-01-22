ae0a982f1153bff87de912cdbc60c5a4640740ce
Adrian S MUresan <adriansmuresan@gmail.com>
2020-01-22 15:49:12 -0600

    Creates a handleChange method that will act onClick on each field in the form

--- catch-of-the-day/src/components/EditFishForm.js
+++ catch-of-the-day/src/components/EditFishForm.js
@@ -1,17 +1,25 @@
 import React from 'react';
 
 class EditFishForm extends React.Component {
+  handleChange = event => {
+    console.log(event.currentTarget.value);
+    //Update that fish
+    // TAke a copy of the current fish
+    console.log(event.currentTarget.name);
+    const updatedfish = {...this.props.fish, [event.currentTarget.name]: event.currentTarget.value}
+  };
+
   render() {
     return (
     <div className="fish-edit">
-        <input type="text" name="name" value={this.props.fish.name} />
-        <input type="text" name="price" value={this.props.fish.price} />
-        <select type="text" name="status" value={this.props.fish.status} >
+        <input type="text" name="name" onChange={this.handleChange} value={this.props.fish.name} />
+        <input type="text" name="price" onChange={this.handleChange} value={this.props.fish.price} />
+        <select type="text" name="status" onChange={this.handleChange} value={this.props.fish.status} >
           <option value="available">Fresh!</option>
           <option value="unavailable">Sold Out!</option>
         </select>
-        <textarea name="desc" value={this.props.fish.desc} />
-        <input type="text" name="image" value={this.props.fish.image} />
+        <textarea name="desc" onChange={this.handleChange} value={this.props.fish.desc} />
+        <input type="text" name="image" onChange={this.handleChange} value={this.props.fish.image} />
     </div>
     )};
 }
