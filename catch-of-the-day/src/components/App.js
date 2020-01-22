df4c293c2006ff8f75660e916cb1b53c111d2ff5
Adrian S MUresan <adriansmuresan@gmail.com>
2020-01-22 15:57:59 -0600

    Adds the updatefish to the inventory and passes it in via props

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -84,7 +84,12 @@ class App extends React.Component {
           </ul>     
         </div>
         <Order fishes={this.state.fishes} order={this.state.order} />
-        <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes} fishes={this.state.fishes} />
+        <Inventory 
+        addFish={this.addFish}
+        updateFish={this.updateFish}
+        loadSampleFishes={this.loadSampleFishes} 
+        fishes={this.state.fishes} 
+        />
       </div>
     )
   }
