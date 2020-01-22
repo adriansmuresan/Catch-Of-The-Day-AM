d8edc8087b29b8c4ea3a4b3eda52ef083cf06b79
Adrian S MUresan <adriansmuresan@gmail.com>
2020-01-22 16:12:41 -0600

    Calls the updateFish method on props in order to update the state

--- catch-of-the-day/src/components/EditFishForm.js
+++ catch-of-the-day/src/components/EditFishForm.js
@@ -2,11 +2,15 @@ import React from 'react';
 
 class EditFishForm extends React.Component {
   handleChange = event => {
-    console.log(event.currentTarget.value);
+    // console.log(event.currentTarget.value);
     //Update that fish
     // TAke a copy of the current fish
-    console.log(event.currentTarget.name);
-    const updatedfish = {...this.props.fish, [event.currentTarget.name]: event.currentTarget.value}
+    // console.log(event.currentTarget.name);
+    const updatedFish = {
+      ...this.props.fish, 
+      [event.currentTarget.name]: event.currentTarget.value
+    };
+    this.props.updateFish(this.props.index, updatedFish );
   };
 
   render() {
