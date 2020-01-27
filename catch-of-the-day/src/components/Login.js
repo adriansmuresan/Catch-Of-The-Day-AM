04eea0d4acd5ba730190c95f9f1de46f1df12467
Adrian S MUresan <adriansmuresan@gmail.com>
2020-01-27 13:45:49 -0600

    Builds the markup for the login, adds proptypes

--- catch-of-the-day/src/components/Login.js
+++ catch-of-the-day/src/components/Login.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Login = () => (
   <nav className="login">
     <h2>Inventory Login</h2>
+    <p>Sign in to manage your store's inventory.</p>
+    <button className="github" onClick={() => this.props.authanticate("Github")}>
+      Log In With GitHub
+    </button>
+    <button className="twitter" onClick={() => this.props.authanticate("Twitter")}>
+      Log In With Twitter
+    </button>
+    <button className="facebook" onClick={() => this.props.authanticate("Facebook")}>
+      Log In With Facebook
+    </button>
   </nav>
 );
+
+Login.propTypes = {
+  authanticate: PropTypes.func.isRequired
+};
 
 export default Login;
